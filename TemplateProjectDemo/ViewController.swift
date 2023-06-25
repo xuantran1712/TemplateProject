be7d95bcc5668c4cb9dedc91ee0a216e9ff37fec
Xuan Tran <xuantran1712@gmail.com>
2023-06-26 01:45:22 +0700

    first commit

---
+++ TemplateProjectDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TemplateProjectDemo
+//
+//  Created by Xuan Tran on 26/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
