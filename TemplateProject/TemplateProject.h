d8e139ff04b1894dcfa691425ea43bb179535ec0
Xuan Tran <xuantran1712@gmail.com>
2023-06-26 01:36:15 +0700

    Initial Commit

---
+++ TemplateProject/TemplateProject.h
@@ -0,0 +1,18 @@
+//
+//  TemplateProject.h
+//  TemplateProject
+//
+//  Created by Xuan Tran on 26/06/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TemplateProject.
+FOUNDATION_EXPORT double TemplateProjectVersionNumber;
+
+//! Project version string for TemplateProject.
+FOUNDATION_EXPORT const unsigned char TemplateProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TemplateProject/PublicHeader.h>
+
+
